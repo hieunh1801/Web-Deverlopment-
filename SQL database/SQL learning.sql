5a4e348cdae891788e484157b345e50645061b1f
Nguyễn Hữu Hiếu <hieunh1801@gmail.com>
2018-03-12 17:32:23 +0700

    Update SQL learning.sql

--- SQL database/SQL learning.sql
+++ SQL database/SQL learning.sql
@@ -50,3 +50,9 @@ INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, 
 -- Cách 2:
 INSERT INTO table_name VALUES (value1, value2, value3, ...);
 -- Chỉ cần thêm các thông tin cần có . Không nhất thiết phải thêm hết. Các ô trống mặc định là NULL
+
+
+----------Update----------
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
